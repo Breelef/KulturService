caa1097626273abcd7ed1c7eb369ad4f1cbbab50
breelef <emil_vinther@hotmail.com>
2022-09-22 14:58:54 +0200

    gitten

--- src/main/java/emil/kulturservice/Controller/UserController.java
+++ src/main/java/emil/kulturservice/Controller/UserController.java
@@ -1,15 +1,14 @@
 package emil.kulturservice.Controller;
 
 import emil.kulturservice.Model.User;
+import emil.kulturservice.Model.Venue;
 import emil.kulturservice.Service.UserInterfaceService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class UserController {
@@ -32,5 +31,17 @@ public class UserController {
     @GetMapping("/getUserByName")
     public ResponseEntity<List<User>> getUserByName(String name){
         return new ResponseEntity<>(userService.findByName(name), HttpStatus.OK);
+    }
+    @PostMapping("/createLike")
+    public ResponseEntity<String> createLike(@RequestParam Long userID, @RequestParam Long venueID){
+        Optional<User> user_ = userService.findById(userID);
+        Optional<Venue> venue_ = venueService.findById(userID);
+        if(user_.isPresent() && venue_.isPresent()){
+            user_.get().getVenuesLiked().add(venue.get());
+            userService.save(user_.get());
+            return new ResponseEntity<>("ok at gamme user: " + userID + "med like venue" + venueID, HttpStatus.OK);
+        }
+        return new ResponseEntity<>("fejl at oprette like", HttpStatus.BAD_REQUEST);
+
     }
 }
